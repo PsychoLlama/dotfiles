8ccff13e546f3dee569640c58ba472ccd2400b83
Jesse Gibson <Jesse_Gibson@me.com>
2017-07-29 17:17:07 -0600

    Add setup script
    Installs a `dotfiles` command into the path.

---
+++ framework-setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash -e
+
+DOTFILES_BIN_DIR=/usr/local/bin
+
+# Resolve the absolute path of the current file.
+pushd `dirname $0` &> /dev/null
+DOTFILES_DIR=`pwd -P`
+popd &> /dev/null
+
+# Symlink the dotfiles command.
+ln -sf "$DOTFILES_DIR/dotfiles.sh" "$DOTFILES_BIN_DIR/dotfiles"
+
+if which dotfiles &> /dev/null; then
+  echo 'It worked! Now you should have a "dotfiles" command.'
+else
+  echo "Hmmm, that didn't work. Is ${DOTFILES_BIN_DIR} in your path?"
+fi
