6fa96015f669ad27f559c513bbed1190a223958f
Jesse Gibson <Jesse_Gibson@me.com>
2018-02-11 11:04:30 -0700

    Move files around and add better tag name
    Puts all the CI related files into their own folder and adds a custom
    git tag name (used for the GitHub releases deploy).

---
+++ ci/predeploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+./ci/setup-s3.sh
+aws s3 sync s3://llama-dotfiles/ ./
+
+# Create a new release target.
+git config --local user.name "Travis CI"
+git config --local user.email "Jesse_Gibson@me.com"
+git tag "$(date +"%Y/%m/%d--%I:%M%p")"
