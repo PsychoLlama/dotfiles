ac17e80f3e0bb463657c0947f742d4ecae474031
Jesse Gibson <Jesse_Gibson@me.com>
2018-02-11 11:20:24 -0700

    Use git-tag-friendly characters
    Turns out colons are invalid characters and slashes ain't too great
    either.

--- ci/predeploy.sh
+++ ci/predeploy.sh
@@ -5,4 +5,4 @@ aws s3 sync s3://llama-dotfiles/ ./
 # Create a new release target.
 git config --local user.name "Travis CI"
 git config --local user.email "Jesse_Gibson@me.com"
-git tag "$(date +"%Y/%m/%d--%I:%M%p")"
+git tag "$(date +"%Y.%m.%d__%I.%M%p")"
