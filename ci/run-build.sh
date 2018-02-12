d8ccecb31ac7c85fa5696e8c64438baa058aa909
Jesse Gibson <Jesse_Gibson@me.com>
2018-02-11 18:39:57 -0700

    Give up on everything
    Abandon the idea of publishing to GitHub releases. This is just
    horrible. Maybe I'll hack out my own solution with a 3rd party secure
    blob hosting provider. This multistage setup just isn't working. 26
    failures is just too much for my weekends.

--- ci/run-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-./ci/setup-s3.sh
-./framework-setup.sh
-cargo build --verbose --release --color always
-aws s3 cp ./target/release/dotfiles \
-  "s3://llama-dotfiles/dotfiles-$TRAVIS_OS_NAME"
