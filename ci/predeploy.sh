8ce916a2c2abec5ec66d7887d307ed652f7b340c
Jesse Gibson <Jesse_Gibson@me.com>
2018-02-11 18:39:57 -0700

    Give up on everything
    Abandon the idea of publishing to GitHub releases. This is just
    horrible. Maybe I'll hack out my own solution with a 3rd party secure
    blob hosting provider. This multistage setup just isn't working. 26
    failures is just too much for my weekends.

--- ci/predeploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-./ci/setup-s3.sh
-aws s3 sync s3://llama-dotfiles/ ./
-
-# Create a new release target.
-# git config --local user.name "Travis CI"
-# git config --local user.email "Jesse_Gibson@me.com"
-# git tag "$(date +"%Y.%m.%d__%I.%M%p")"
