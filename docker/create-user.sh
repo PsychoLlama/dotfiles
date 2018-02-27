9e13f4c0d53b7faa27e7689fc9b9ba506c88df91
Jesse Gibson <Jesse_Gibson@me.com>
2018-02-26 19:51:34 -0700

    Allow passwordless `chsh -s ...`
    Mostly by copy/pasting code from the internet. I've become what I hate
    most. I need my docker environment!!!

--- docker/create-user.sh
+++ docker/create-user.sh
@@ -7,7 +7,10 @@ apt-get install -y git sudo
 adduser --disabled-password --gecos overlord overlord
 usermod -aG sudo overlord
 
-# Allow sudo without a password.
+# Allow passwordless `sudo`.
 echo 'overlord ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers.d/overlord
+
+# Allow passwordless `chsh`.
+echo 'auth sufficient pam_wheel.so trust group=sudo' >> /etc/pam.d/chsh
 
 su -c /var/dotfiles-setup/install-dotfiles.sh overlord
