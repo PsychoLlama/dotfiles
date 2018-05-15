dd02ea150de6801f9c48528f988f04773cb89358
Jesse Gibson <JesseTheGibson@gmail.com>
2018-05-14 22:47:08 -0600

    Add eject script
    I made this tool for working in a vm, but it's been so useful I wanna
    make it part of the framework. It copies all the files into a
    timestamped directory and tars it up.
    
    Super great for things you don't wanna commit, but still need to keep
    around in automated backups or when you're about to revert a vm.

---
+++ eject.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090
+set -e
+
+parachute=parachute.tar.gz
+files=(
+  ~/.vim/backup/
+  ~/.vim/undodir/
+  ~/.vim/metrics.json
+  ~/.node_repl_history
+  ~/.python_history
+  ~/.zsh_history
+  ~/.z
+)
+
+function warn {
+  echo -e "WARN: $*" >&2
+}
+
+manifest=~/dotfiles-env/eject-manifest.sh
+if [[ -f "$manifest" && -e "$manifest" ]]; then
+  source "$manifest"
+  files=("${files[@]}" "${DOTFILES_MANIFEST[@]}")
+fi
+
+# Timestamp as <date>--<time>
+# Example: 2018-05-18--16-05
+date="$(date +'%Y-%m-%d--%H-%M')"
+target="$(dotfiles dir)/$date"
+mkdir -p "$target"
+
+for file in "${files[@]}"; do
+  # Skip files that don't exist.
+  if [[ ! -e "$file" ]]; then
+    warn "No file or directory. Skipping $file"
+    continue
+  fi
+
+  # `cp -R` handles directories with trailing slashes unexpectedly.
+  # TODO: make sure this syntax won't kill us all someday.
+  file="${file%\/}"
+
+  # Preserving file attributes & follow symlinks.
+  cp -LRp "$file" "$target/"
+done
+
+tar -czf "$parachute" "$target" &> /dev/null
+rm -rf "$target"
+
+echo "$parachute"
