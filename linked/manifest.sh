1972d3791dd2b5a13259e77554f53ed4e39f7a98
Jesse Gibson <JesseTheGibson@gmail.com>
2018-07-12 22:41:03 -0600

    Add support for environment-specific symlinks
    I've been wanting this feature for months! Finally, I have the ability
    to add custom symlinks in my dotfiles-env preset. Stuff like vagrant
    config, work-specific tmuxinator scripts, computer-specific applescript
    automations, all that stuff. I can't believe it was that easy.

--- linked/manifest.sh
+++ linked/manifest.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC1090
+set -e
+
 function get_dotfiles_manifest {
   echo ~/.tmuxinator/edit.yml .tmuxinator/edit.yml
   echo ~/.config/nvim/init.vim init.vim
   echo ~/.gitconfig .gitconfig
   echo ~/.tmux.conf .tmux.conf
   echo ~/.zshrc .zshrc
+
+  local symlink_manifest=~/dotfiles-env/symlink-manifest.sh
+  if [[ -f "$symlink_manifest" ]]; then
+    source "$symlink_manifest"
+  fi
 }
