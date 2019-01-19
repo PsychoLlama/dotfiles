3cec0a532e08ac09588027033c767e8d8eb807aa
Jesse Gibson <JesseTheGibson@gmail.com>
2019-01-18 17:04:09 -0700

    Silence login info
    I guess I don't configure new computers often enough to notice this.
    Silence the shell login information.

--- linked/manifest.sh
+++ linked/manifest.sh
@@ -5,6 +5,7 @@ set -e
 function get_dotfiles_manifest {
   echo ~/.tmuxinator/edit.yml .tmuxinator/edit.yml
   echo ~/.config/nvim/init.vim init.vim
+  echo ~/.hushlogin empty-file.txt
   echo ~/.gitconfig .gitconfig
   echo ~/.tmux.conf .tmux.conf
   echo ~/.ripgrep .ripgrep
