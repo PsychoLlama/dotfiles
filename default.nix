b012e5e1e1eb0179c4f044acff54661ec5dbc38a
Jesse Gibson <JesseTheGibson@gmail.com>
2020-04-06 22:36:31 -0600

    Replace several rust packages with nix
    All but two cargo packages can be fully replaced with Nix. The others
    don't have upstream nix derivations yet.

--- default.nix
+++ default.nix
@@ -2,5 +2,6 @@ with (import <nixpkgs> {});
 
 {
   inherit rustup shellcheck zsh travis tmux tmuxinator vim-vint yarn;
-  inherit unzip glow antigen neovim;
+  inherit unzip glow antigen neovim ripgrep fd viu bat skim pastel;
+  inherit hexyl;
 }
