6372900b853e65489d4298598a4802657994c0b6
Jesse Gibson <JesseTheGibson@gmail.com>
2020-05-11 10:24:00 -0600

    Replace zoxide pkg with upstream definition
    Zoxide was added to nixpkgs. I don't need to maintain my own package
    definition anymore.

--- default.nix
+++ default.nix
@@ -3,9 +3,8 @@ with (import <nixpkgs> {});
 {
   inherit rustup shellcheck zsh travis tmux tmuxinator vim-vint yarn;
   inherit unzip glow antigen neovim ripgrep fd viu bat skim pastel;
-  inherit hexyl;
+  inherit hexyl zoxide;
 
   fnm = import ./pkgs/fast-node-manager/default.nix;
   git-delta = import ./pkgs/git-delta.nix;
-  zoxide = import ./pkgs/zoxide.nix;
 }
