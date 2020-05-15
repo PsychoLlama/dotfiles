1bad6b27e1f8515fc4cba76356f99b7ff4634957
Jesse Gibson <JesseTheGibson@gmail.com>
2020-05-15 14:07:37 -0600

    Replace git-delta install with nix upstream
    I can't believe how many packages are supported by nix! This is insane!
    
    Anyway, that's one less derivation I have to maintain myself. Hooray!

--- default.nix
+++ default.nix
@@ -5,6 +5,7 @@ with (import <nixpkgs> {});
   inherit unzip glow antigen neovim ripgrep fd viu bat skim pastel;
   inherit hexyl zoxide;
 
+  inherit (gitAndTools) delta;
+
   fnm = import ./pkgs/fast-node-manager/default.nix;
-  git-delta = import ./pkgs/git-delta.nix;
 }
