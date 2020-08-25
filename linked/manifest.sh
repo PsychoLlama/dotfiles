61051ef80150aa4dc483f3b50f287d451bb6cf3b
Jesse Gibson <JesseTheGibson@gmail.com>
2020-08-24 19:20:20 -0600

    Add `procs` command
    It's a `ps` replacement. It's cool.

--- linked/manifest.sh
+++ linked/manifest.sh
@@ -7,6 +7,7 @@ function get_dotfiles_manifest {
   echo ~/.config/alacritty.yml alacritty.yml
   echo ~/.config/nvim/init.vim init.vim
   echo ~/.hushlogin empty-file.txt
+  echo ~/.procs.toml procs.toml
   echo ~/.gitconfig .gitconfig
   echo ~/.tmux.conf .tmux.conf
   echo ~/.ripgrep .ripgrep
