10341a8c3e73bb3379de5dbe876ca5d2a8a9cee3
Jesse Gibson <JesseTheGibson@gmail.com>
2020-12-21 16:51:32 -0800

    Add Dog DNS client
    Ever used dig? It sucks. Long live dog :metal:

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ with (import <nixpkgs> {});
 {
   inherit rustup shellcheck zsh travis tmux tmuxinator vim-vint yarn;
   inherit unzip glow antigen neovim ripgrep fd viu bat skim pastel;
-  inherit hexyl zoxide procs;
+  inherit hexyl zoxide procs dogdns;
 
   inherit (gitAndTools) delta;
 
