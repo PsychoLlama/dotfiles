da625999f037dae132e5b1c1cb3bcbc05a8c72e5
Jesse Gibson <JesseTheGibson@gmail.com>
2020-12-23 19:33:58 -0800

    Add script to generate a nix vim plugin lockfile
    I'm in the process of migrating my bucket o' hacks `nixplug` manager to
    a more standardized approach. Nixpkgs supports vim packages (`:h
    packages`), which would allow me to version my plugins very explicitly
    and instantly rollback any breaking upgrades.
    
    That's all fine, but nixpkgs barely supports any vim plugins and I'm not
    confident enough in my upstreaming abilities to contribute those yet,
    nor would I want to be solely dependent on a 3rd party to manage all my
    vim plugins for me. Especially considering some of my plugins are
    half-baked.
    
    Although there are tools to automatically build a vim plugin
    (`vimUtils.buildVimPlugin`), you still need the infrastructure to pin
    revisions and generate hashes. You could technically skim by on the
    master branch (what I'm currently doing), but you miss out on a lot of
    features and it's more risky.
    
    Instead of using master, I chose to implement my own lockfile generator
    akin to what they're doing upstream. It's not as fancy, but it serves
    exactly what I need, and it means I'm still not dependent on a 3rd
    party.

---
+++ pkgs/vim-plugins/plugin-names.js
@@ -0,0 +1,45 @@
+module.exports = [
+  // Filetype plugins
+  'davinche/godown-vim',
+  'leafgarland/typescript-vim',
+  'PsychoLlama/debrief.vim',
+  'hashivim/vim-terraform',
+  'jparise/vim-graphql',
+  'rust-lang/rust.vim',
+  'tpope/vim-markdown',
+  'cespare/vim-toml',
+  'othree/yajs.vim',
+  'chr4/nginx.vim',
+  'LnL7/vim-nix',
+  'mxw/vim-jsx',
+
+  // ALL HAIL TIM POPE
+  'tpope/vim-commentary',
+  'tpope/vim-fugitive',
+  'tpope/vim-surround',
+  'tpope/vim-endwise',
+  'tpope/vim-repeat',
+
+  // Enhancements ✨
+  'AndrewRadev/splitjoin.vim',
+  'jiangmiao/auto-pairs',
+  'machakann/vim-swap',
+  'mbbill/undotree',
+  'w0rp/ale',
+
+  // Navigation
+  'PsychoLlama/alternaut.vim',
+  'PsychoLlama/navitron.vim',
+  'PsychoLlama/teleport.vim',
+  'PsychoLlama/further.vim',
+  'lotabout/skim.vim',
+  'lotabout/skim',
+
+  // Style 🦄
+  'airblade/vim-gitgutter',
+  'joshdick/onedark.vim',
+
+  // Fun 🎮
+  'PsychoLlama/conway.vim',
+  'PsychoLlama/snake.vim',
+];
