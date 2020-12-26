8cb969a5600c4cedf8da02b09d4a642a3ac6ba47
Jesse Gibson <JesseTheGibson@gmail.com>
2020-12-25 18:26:26 -0800

    Give up on 24h time
    In theory it would help reduce ambiguity and make time math easier. In
    practice, I just ended up doing more time math for conversions and
    spending far too much effort reading microwave manuals.
    
    I'm reverting to 12h time.

--- utils/print-time.sh
+++ utils/print-time.sh
@@ -2,10 +2,13 @@
 
 # Print a fancy date, like "18:38 Tuesday"
 function tmux_get_time_status {
-  local time="$(date +%R)"
+  local time="$(date +%l:%M)"
   local day="$(date +%A)"
 
-  echo "$time $day"
+  # Cross-platform lowercase am/pm requires some slight hackery.
+  local am_pm="$(date +%p | tr '[:upper:]' '[:lower:]')"
+
+  echo "${time/ /}${am_pm} ${day}"
 }
 
 tmux_get_time_status
