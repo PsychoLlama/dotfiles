4c7ed9ed27c2ee04c98427b5f6f472d5a27ff587
Jesse Gibson <JesseTheGibson@gmail.com>
2021-01-24 18:35:45 -0800

    Add pv tool
    I recently learned about `pv` for viewing real-time progress of file
    transfers over a shell pipe. You ever stare at a `dd` command running
    for hours wondering how much longer it'll take? `pv` knows.

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ callPackage ./pkgs/zsh-plugins {} // {
   inherit (gitAndTools) delta;
   inherit rustup cargo-edit shellcheck zsh tmux tmuxinator vim-vint yarn;
   inherit unzip glow ripgrep fd viu bat skim pastel hexyl zoxide procs;
-  inherit dogdns jq miniserve zathura;
+  inherit dogdns jq miniserve pv zathura;
 
   fnm = import ./pkgs/fast-node-manager/default.nix;
   neovim = neovim.override {
