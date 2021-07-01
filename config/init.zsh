68965cccd7b01ae86d25b3b63b9fa7f442fd4f25
Jesse Gibson <JesseTheGibson@gmail.com>
2021-07-01 15:07:07 -0700

    Import zsh config from NixOS
    This is the zsh config I've been using since switching to NixOS.
    I wrote this one from scratch, so any similarities to the old config
    were strictly from nostalgia.

--- config/init.zsh
+++ config/init.zsh
@@ -1,224 +1,146 @@
-### Variables ###
-export VIRTUAL_ENV_DISABLE_PROMPT=1
-export SKIM_DEFAULT_COMMAND='fd'
-export COLORTERM=truecolor
-export ZSH=~/.nix-profile/share/oh-my-zsh
-export EDITOR=nvim
-export PATH=~/.cargo/bin:"$PATH"
-export MANPAGER='nvim -c "setfiletype man" -'
-export RIPGREP_CONFIG_PATH=~/.ripgrep
-export BAT_STYLE=changes
-export BAT_THEME=TwoDark
-export NIX_IGNORE_SYMLINK_STORE=1 # Catalina ruins everything.
+prompt off # Disable default prompt settings (i.e. RPS1)
 
-
-### Aliases ###
-alias g='git'
-alias b='git branch'
-alias :qa='tmux kill-session 2> /dev/null || exit'
-alias empty='empty-trash'
-alias ch='git checkout'
-alias D='cd ~/Desktop'
-alias c='git commit'
-alias H='git diff HEAD'
-alias HH='git diff HEAD~1'
-alias HHH='git diff HEAD~2'
-
-# Branch "dirstack"
-alias b1='git checkout "$(git branch-history 1 | tail -1)"'
-alias b2='git checkout "$(git branch-history 2 | tail -1)"'
-alias b3='git checkout "$(git branch-history 3 | tail -1)"'
-alias b4='git checkout "$(git branch-history 4 | tail -1)"'
-alias b5='git checkout "$(git branch-history 5 | tail -1)"'
-alias b6='git checkout "$(git branch-history 6 | tail -1)"'
-alias b7='git checkout "$(git branch-history 7 | tail -1)"'
-alias b8='git checkout "$(git branch-history 8 | tail -1)"'
-alias b9='git checkout "$(git branch-history 9 | tail -1)"'
-
-# Fancy alternatives
-alias cat=bat
-
-### Functions ###
-function mkcd {
-  mkdir -p "$1"
-  cd "$1" || return 1
-}
-
-# Open a development environment in the current directory
-function edit {
-  local session_name="$1"
-
-  if [[ -z "$session_name" ]]; then
-    session_name="$(basename "$PWD")"
-  fi
-
-  tmuxinator start edit -n "$session_name"
-}
-
-# Poke around another project without the commitment.
-function inspect {
-  local remote="$1"
-  local target="$(mktemp -d)"
-
-  if [[ "${1:0:4}" != http ]] && [[ "${1:0:3}" != git ]]; then
-    remote="https://github.com/$remote"
-  fi
-
-  git clone --quiet --depth=1 "$remote" "$target"
-  nvim "$target"
-  rm -rf "$target"
-}
-
-# Faster `git status`
-function s {
-  if ! git rev-parse --is-inside-work-tree &> /dev/null; then
-    echo "Not a git repo."
-    return 1
-  fi
-
-  git status
-}
-
-# tmux shorthand
-function t {
-  # Arguments? Treat `t` like an alias.
-  if [[ $# != 0 ]]; then
-    tmux "$@"
-    return $?
-  fi
-
-  if [[ -n "$TMUX" ]]; then
-    echo "Chill out dude. One tmux is enough." 1>&2
-    return 1
-  fi
-
-  # Attach to the most recent session, or create one.
-  local recent_session="$(tmux ls 2> /dev/null | awk 'END {print $1}' | sed 's/://')"
-
-  if [[ -z "$recent_session" ]]; then
-    tmux new
-    return $?
-  fi
-
-  tmux attach -t "$recent_session"
-  return $?
-}
-
-# tmux attach with skim
-function ta {
-  local session="$(tmux list-sessions -F '#{session_name}' | sk)"
-  if [[ -z "$session" ]]; then
-    return 1
-  fi
-
-  if [[ -z "$TMUX" ]]; then
-    tmux attach -t "$session"
-  else
-    tmux switch-client -t "$session"
-  fi
-}
-
-function gg {
-  rg "$@" --ignore-file <(echo "__tests__\ntests\nflow-typed")
-}
-
-# Like `nvim` but better
-function v {
-  if [[ "$#" == "0" ]]; then
-    nvim .
-  else
-    nvim -p "$@"
-  fi
-}
-
-function vs {
-  if [[ "$#" == "0" ]]; then
-    echo "WRONG! Try again."
-    return 1
-  fi
-
-  nvim -p $(gg "$@" -l)
-}
-
-function vh {
-  if ! git rev-parse --is-inside-work-tree &> /dev/null; then
-    echo 'Not a repo, loser.'
-    return 1
-  fi
-
-  local repo_root="$(git rev-parse --show-toplevel)"
-  local filepaths=()
-
-  git diff HEAD --name-only | while read -r filepath; do
-    filepaths=($filepaths "$repo_root/$filepath")
-  done
-
-  if [[ "${#filepaths[@]}" == 0 ]]; then
-    echo 'No changes :/'
-    return 1
-  fi
-
-  v "${filepaths[@]}"
-}
-
-function vf {
-  local cache_file=/tmp/vf-last-result
-  local file
-
-  case "$1" in
-    '--last' | '-l')
-      file="$(command cat "$cache_file")"
-
-      if [[ -z "$file" ]]; then
-        echo "There's no find history :/" >&2
-        return 1
-      fi
-
-      nvim "$file"
-      return $?
-      ;;
-    *)
-      if file="$(fd | sk)"; then
-        echo "${PWD}/$file" > "$cache_file"
-        echo "$file"
-        nvim "$file"
-      fi
-      ;;
-  esac
-}
-
-function cf {
-  local dir="$(fd --type d | sk)"
-
-  if [[ -z "$dir" ]]; then
-    return 1
-  fi
-
-  cd "$dir"
-}
-
-source ~/.nix-profile/etc/profile.d/nix.sh
-
-plugins=(docker)
-source ~/.nix-profile/share/oh-my-zsh/oh-my-zsh.sh
-source ~/.nix-profile/share/zsh-rustup-completion/rustup.plugin.zsh
-source ~/.nix-profile/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source ~/.nix-profile/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-source ~/.nix-profile/share/llama.zsh-theme/llama.zsh-theme
-
-# Use fnm-managed node installation.
-if [[ -d ~/.fnm ]]; then
-  export PATH="$HOME/.fnm:$PATH"
-  eval "$(fnm env)"
-fi
-
-# Initialize zoxide (`z` command)
+eval "$(starship init zsh)"
 eval "$(zoxide init zsh)"
 
-# Check for computer-specific shell utils.
-if [[ -e ~/dotfiles-env/.zshrc ]]; then
-  source ~/dotfiles-env/.zshrc
+
+##################
+# Shell Settings #
+##################
+
+# Don't stifle error codes in pipelines.
+setopt pipefail
+
+# Change directories without requiring `cd`.
+setopt auto_cd
+
+# Automatically put navigation history in the dir stack.
+setopt auto_pushd pushd_ignore_dups
+
+# Don't store duplicates, inline functions, or commands starting with a space
+# character (incognito mode) in zsh history.
+setopt hist_ignore_all_dups hist_no_functions hist_ignore_space
+
+# Prettify entries and immediately store them in history.
+setopt hist_reduce_blanks share_history
+
+# Allow comments in interactive shells.
+setopt interactive_comments
+
+
+###########
+# Aliases #
+###########
+
+# Easily navigate the dir stack.
+for i in {1..5}; do
+  alias "$i"="cd +$i"
+done
+
+
+##################
+# Key Management #
+##################
+
+export SSH_AUTH_SOCK=/tmp/ssh-agent.sock
+
+if [[ ! -S "$SSH_AUTH_SOCK" ]]; then
+  eval "$(ssh-agent -a "$SSH_AUTH_SOCK" > /dev/null)"
 fi
 
-### Completions ###
-compdef t=tmux
+
+#########################
+# Convenience Functions #
+#########################
+
+# Open the editor.
+function v {
+  if (( $# > 0 )); then
+    nvim -p "$@"
+  else
+    nvim .
+  fi
+}
+
+# Fuzzy find and edit a file.
+function vf {
+  local result="$(fd --type file | sk)"
+
+  if [[ -z "$result" ]]; then
+    return
+  fi
+
+  nvim "$result"
+}
+
+# Jump down to a directory using fuzzy search.
+function cf {
+  local result="$(fd --type directory | sk)"
+
+  if [[ -z "$result" ]]; then
+    return
+  fi
+
+  cd "$result"
+}
+
+# Show git status.
+function s {
+  if git rev-parse --is-inside-work-tree &> /dev/null; then
+    git status
+  else
+    echo 'Not a git repo.' >&2
+  fi
+}
+
+# `mkdir` and `cd` in one move.
+function mkcd {
+  mkdir --parents "$1"
+  cd "$1"
+}
+
+# Open modified files in the editor.
+function vh {
+  local files=($(git ls-files --modified --others --exclude-standard 2> /dev/null))
+
+  if [[ "$?" != 0 ]] || (( ${#files} == 0 )); then
+    echo 'No modified files.' > /dev/stderr
+    return 1
+  fi
+
+  nvim -p "${files[@]}"
+}
+
+# Open search results in the editor.
+function vs {
+  local files=($(rg "$@" -l))
+
+  if (( ${#files} == 0 )); then
+    echo 'No results.' > /dev/stderr
+    return 1
+  fi
+
+  nvim -p "${files[@]}"
+}
+
+# Encrypt stdin using public keys from GitHub.
+function encrypt {
+  if [[ -z "$1" ]]; then
+    echo 'Expected a username.' > /dev/stderr
+    return 1
+  fi
+
+  local args=('-a')
+  curl -sSL "https://github.com/$1.keys" | while read -r identity; do
+    local file="$(mktemp)"
+    echo "$identity" > "$file"
+    args=("${args[@]}" '-R' "$file")
+  done
+
+  rage "${args[@]}" -
+}
+
+# Decrypt stdin using the SSH private key.
+function decrypt {
+  rage -d -i ~/.ssh/id_ed25519 -
+}
