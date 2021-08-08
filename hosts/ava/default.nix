0924a90fef91db6c5bb0112d42e26a92225fff57
Jesse Gibson <JesseTheGibson@gmail.com>
2021-08-07 23:11:41 -0700

    Sync files to TrueNAS over Syncthing

--- hosts/ava/default.nix
+++ hosts/ava/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ unstable, ... }:
 
 {
   imports = [ ./hardware-configuration.nix ];
@@ -26,6 +26,38 @@
   dotfiles.user = {
     account = "overlord";
     fullName = "Jesse Gibson";
+  };
+
+  services.syncthing = {
+    enable = true;
+    package = unstable.syncthing-cli;
+
+    user = "overlord";
+    group = "users";
+    dataDir = "/home/overlord";
+
+    declarative = {
+      # A general-purpose box for reliable storage.
+      folders."/home/overlord/attic" = {
+        id = "attic";
+        devices = [ "file-server" "phone" ];
+        label = "Attic";
+      };
+
+      devices = {
+        file-server = {
+          addresses = [ "dynamic" ];
+          id =
+            "73NJCWB-2R4WVZC-IJ2IRPF-7XQH7P4-DM43L7L-HE4HV5D-MKU4Z4V-PL5QVQ5";
+        };
+
+        phone = {
+          addresses = [ "dynamic" ];
+          id =
+            "YTUVZSZ-V4TOBKD-SCKD4B6-AOW5TMT-PGCLJO6-7MLGZII-FOYC7JO-LGP62AX";
+        };
+      };
+    };
   };
 
   system.stateVersion = "20.09";
