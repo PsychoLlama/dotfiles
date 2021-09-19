2e2d876f4d3f4aafe664605fd65db23e60d70616
Jesse Gibson <JesseTheGibson@gmail.com>
2021-09-18 20:16:55 -0700

    Add Brave browser
    I use it for the native IPFS integration and the occasional instance
    where I need Chromium (generally video conferencing stuff).

--- modules/apps.nix
+++ modules/apps.nix
@@ -3,34 +3,51 @@
 let
   df = config.dotfiles;
   cfg = df.apps;
+  browsers = cfg.browsers;
 
 in {
   options.dotfiles.apps = with lib; {
-    firefox = {
-      enable = mkOption {
-        type = types.bool;
-        description = "Enable Firefox";
-        default = df.kitchen-sink.enable;
+    browsers = {
+      firefox = {
+        enable = mkOption {
+          type = types.bool;
+          description = "Enable Firefox";
+          default = df.kitchen-sink.enable;
+        };
+
+        package = mkOption {
+          type = types.package;
+          description = "Which firefox package to use";
+          default = unstable.firefox;
+        };
       };
 
-      package = mkOption {
-        type = types.package;
-        description = "Which firefox package to use";
-        default = unstable.firefox;
-      };
-    };
+      brave = {
+        enable = mkOption {
+          type = types.bool;
+          description = "Enable the Brave browser";
+          default = df.kitchen-sink.enable;
+        };
 
-    torbrowser = {
-      enable = mkOption {
-        type = types.bool;
-        description = "Enable the Tor browser";
-        default = df.kitchen-sink.enable;
+        package = mkOption {
+          type = types.package;
+          description = "Which Brave package to use";
+          default = unstable.brave;
+        };
       };
 
-      package = mkOption {
-        type = types.package;
-        description = "Which Tor browser package to use";
-        default = unstable.torbrowser;
+      tor = {
+        enable = mkOption {
+          type = types.bool;
+          description = "Enable the Tor browser";
+          default = df.kitchen-sink.enable;
+        };
+
+        package = mkOption {
+          type = types.package;
+          description = "Which Tor browser package to use";
+          default = unstable.torbrowser;
+        };
       };
     };
 
@@ -51,8 +68,9 @@ in {
 
   config = with lib; {
     environment.systemPackages =
-      (if cfg.firefox.enable then [ cfg.firefox.package ] else [ ])
-      ++ (if cfg.torbrowser.enable then [ cfg.torbrowser.package ] else [ ])
+      (if browsers.firefox.enable then [ browsers.firefox.package ] else [ ])
+      ++ (if browsers.tor.enable then [ browsers.tor.package ] else [ ])
+      ++ (if browsers.brave.enable then [ browsers.brave.package ] else [ ])
       ++ (if cfg.zathura.enable then [ cfg.zathura.package ] else [ ]);
   };
 }
