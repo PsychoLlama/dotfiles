9715c2136c80729eef4419366134cf25db456722
Jesse Gibson <JesseTheGibson@gmail.com>
2022-06-20 17:44:07 -0700

    Add shortcut for previewing markdown

--- config/neovim.lua
+++ config/neovim.lua
@@ -66,6 +66,7 @@ vim.api.nvim_set_keymap('i', '<s-tab>', 'editor#mappings#tab_completion(v:true)'
 -- Misc
 vim.api.nvim_set_keymap('n', '<esc>', ':nohlsearch<cr><esc>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>;', ':call editor#mappings#test()<cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>m', ':MarkdownPreview<cr>', { noremap = true, silent = true })
 
 -- Markdown previewer
 vim.g.mkdp_browser = 'firefox'
