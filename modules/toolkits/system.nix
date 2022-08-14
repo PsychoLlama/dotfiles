971d7ba7214524d4d47d0a6caedf4d1957564fd4
Jesse Gibson <JesseTheGibson@gmail.com>
2022-08-14 13:45:56 -0700

    Add duf for disk management
    It's kinda nice to look at the pretty colors while imagining them in
    btrfs.

--- modules/toolkits/system.nix
+++ modules/toolkits/system.nix
@@ -23,7 +23,7 @@ in {
     with unstable;
     mkMerge [
       (mkIf cfg.enable {
-        environment.systemPackages = [ bottom man-pages ncspot rage ];
+        environment.systemPackages = [ bottom man-pages ncspot rage duf ];
       })
 
       (mkIf cfg.linux.enable {
