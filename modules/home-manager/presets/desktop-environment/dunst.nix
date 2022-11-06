c3171777953996a7c23dd92b169a86e5b6932f35
Jesse Gibson <JesseTheGibson@gmail.com>
2022-11-06 14:21:40 -0800

    Use declarative config for dunst
    Most config values were using the default, according to `dunst(5)`.

--- modules/home-manager/presets/desktop-environment/dunst.nix
+++ modules/home-manager/presets/desktop-environment/dunst.nix
@@ -9,6 +9,44 @@ in with lib; {
   config.services.dunst = mkIf cfg.enable {
     enable = true;
     package = pkgs.unstable.dunst;
-    configFile = ../../../../config/dunst.cfg;
+
+    settings = {
+      global = {
+        follow = "keyboard";
+        font = "Monospace 12";
+        frame_color = "#3f3f3f";
+        frame_width = 2;
+        geometry = "300x5-30+20";
+        idle_threshold = 120;
+        markup = "full";
+        sort = true;
+
+        max_icon_size = 32;
+        min_icon_size = 0;
+
+        mouse_left_click = "do_action, close_current";
+        mouse_middle_click = "close_all";
+        mouse_right_click = "close_current";
+      };
+
+      urgency_low = {
+        background = "#1e1e1e";
+        foreground = "#6f6f6f";
+        timeout = 10;
+      };
+
+      urgency_normal = {
+        background = "#1e1e1e";
+        foreground = "#bfc5ce";
+        timeout = 10;
+      };
+
+      urgency_critical = {
+        background = "#be5046";
+        foreground = "#ffffff";
+        frame_color = "#e06c75";
+        timeout = 0;
+      };
+    };
   };
 }
