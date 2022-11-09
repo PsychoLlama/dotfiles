1b215d3133e76ca5fff61678803426c14276e575
Jesse Gibson <JesseTheGibson@gmail.com>
2022-11-08 21:07:41 -0800

    Automatically generate missing program modules
    The formula for creating a configurable program module is
    straightforward enough to abstract into a module.

--- modules/home-manager/programs/default.nix
+++ modules/home-manager/programs/default.nix
@@ -1,14 +1,31 @@
-{
+let
+  # Define a simple `programs.<name>` module that installs and configures an
+  # arbitrary package.
+  makeProgramModule = pkgName:
+    { config, lib, pkgs, ... }:
+    let cfg = config.programs.${pkgName};
+
+    in with lib; {
+      options.programs.${pkgName} = {
+        enable = mkEnableOption "Whether to install ${pkgName}";
+        package = mkPackageOption pkgs pkgName { };
+      };
+
+      config.home.packages = mkIf cfg.enable [ cfg.package ];
+    };
+
+in {
   imports = [
-    ./dive.nix
-    ./dogdns.nix
-    ./fd.nix
-    ./miniserve.nix
-    ./nmap.nix
-    ./sshfs.nix
-    ./termshark.nix
     ./w3m.nix
-    ./whois.nix
-    ./xh.nix
+
+    (makeProgramModule "dive")
+    (makeProgramModule "dogdns")
+    (makeProgramModule "fd")
+    (makeProgramModule "miniserve")
+    (makeProgramModule "nmap")
+    (makeProgramModule "sshfs")
+    (makeProgramModule "termshark")
+    (makeProgramModule "whois")
+    (makeProgramModule "xh")
   ];
 }
