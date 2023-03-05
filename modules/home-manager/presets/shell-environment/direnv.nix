112776d478be00ddb1555527bb56728ae37cea8c
Jesse Gibson <JesseTheGibson@gmail.com>
2023-03-05 18:55:05 -0500

    Automatically allow envs for personal projects
    Every project with an `.envrc` under my username is allowed by default.

--- modules/home-manager/presets/shell-environment/direnv.nix
+++ modules/home-manager/presets/shell-environment/direnv.nix
@@ -6,5 +6,9 @@ let cfg = config.presets.direnv;
 
 in {
   options.presets.direnv.enable = mkEnableOption "Install and configure direnv";
-  config.programs.direnv = mkIf cfg.enable { enable = true; };
+  config.programs.direnv = mkIf cfg.enable {
+    enable = true;
+    config.whitelist.prefix =
+      [ "${config.home.homeDirectory}/projects/psychollama" ];
+  };
 }
