b2ac212f97af67f5f45a1434633a655aa04b1fd4
Jesse Gibson <JesseTheGibson@gmail.com>
2023-03-10 22:46:53 -0500

    Accelerate direnv with nix flakes
    I'm using the `nix-direnv` plugin to improve speed of `direnv`. Just
    about all my uses of `direnv` are flakes.

--- modules/home-manager/presets/shell-environment/direnv.nix
+++ modules/home-manager/presets/shell-environment/direnv.nix
@@ -8,6 +8,7 @@ in {
   options.presets.direnv.enable = mkEnableOption "Install and configure direnv";
   config.programs.direnv = mkIf cfg.enable {
     enable = true;
+    nix-direnv.enable = true;
     config.whitelist.prefix =
       [ "${config.home.homeDirectory}/projects/psychollama" ];
   };
