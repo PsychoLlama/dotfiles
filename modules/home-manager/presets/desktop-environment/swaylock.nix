c168d198b5bd9bac2507c252d1abf7bdd1231268
Jesse Gibson <JesseTheGibson@gmail.com>
2023-03-11 14:43:58 -0500

    Style Swaylock with NixOS theme palette
    Now it matches the rest of my OS color scheme. I'm not thrilled with the
    styling, but it's better.

--- modules/home-manager/presets/desktop-environment/swaylock.nix
+++ modules/home-manager/presets/desktop-environment/swaylock.nix
@@ -2,7 +2,10 @@
 
 with lib;
 
-let cfg = config.presets.swaylock;
+let
+  inherit (config.theme) palette;
+  cfg = config.presets.swaylock;
+  semi-black = "${palette.normal.black}d9"; # alpha(0.85)
 
 in {
   options.presets.swaylock.enable =
@@ -10,9 +13,29 @@ in {
 
   config.programs.swaylock.settings = mkIf cfg.enable {
     image = "attic/images/wallpapers/current";
-    color = "000000";
     daemonize = true;
     show-failed-attempts = true;
     ignore-empty-password = true;
+
+    color = palette.normal.black;
+    line-color = palette.normal.black;
+
+    ring-color = semi-black;
+    inside-color = semi-black;
+    text-color = palette.normal.red;
+    key-hl-color = palette.normal.green;
+    bs-hl-color = palette.normal.red;
+
+    ring-clear-color = palette.bright.white;
+    inside-clear-color = semi-black;
+    text-clear-color = palette.bright.white;
+
+    ring-ver-color = palette.normal.yellow;
+    inside-ver-color = semi-black;
+    text-ver-color = palette.normal.yellow;
+
+    ring-wrong-color = palette.normal.red;
+    inside-wrong-color = semi-black;
+    text-wrong-color = palette.normal.red;
   };
 }
