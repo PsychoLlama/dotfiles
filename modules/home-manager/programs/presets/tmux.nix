a0f66d68b2702eb486461d3151f73bb097de7471
Jesse Gibson <JesseTheGibson@gmail.com>
2024-03-05 22:08:04 -0500

    Always pull nushell from module package
    This allows me to replace nushell with things like `nushellFull` for
    that sweet, sweet dataframe goodness without updating it in 10 places.

--- modules/home-manager/programs/presets/tmux.nix
+++ modules/home-manager/programs/presets/tmux.nix
@@ -18,7 +18,7 @@ in {
       escapeTime = 0;
       historyLimit = 100000;
       keyMode = "vi";
-      shell = "${pkgs.unstable.nushell}/bin/nu";
+      shell = "${config.programs.nushell.package}/bin/nu";
       extraConfig = builtins.readFile ../../../../config/tmux.conf;
     };
 
