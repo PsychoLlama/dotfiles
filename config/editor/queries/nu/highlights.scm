9b67a7f348a1b33522149009a349d0e264aa2397
Jesse Gibson <JesseTheGibson@gmail.com>
2024-06-02 14:07:31 -0400

    Restore nushell TS queries to working state
    The NixOS upgrade brought a new version of the Nushell grammar that
    broke my highlight and indent queries. The names of AST nodes changed
    (for the better, IMO) but that meant my old queries were throwing an
    error on every keystroke. Not great.
    
    This fixes the queries enough to restore functionality.

--- config/editor/queries/nu/highlights.scm
+++ config/editor/queries/nu/highlights.scm
@@ -6,20 +6,22 @@
 
 ; Basic values
 (comment) @comment
-(string) @string
-(number_literal) @number
-(bool_literal) @bool
-(value_path) @variable
-(type) @type
+(val_string) @string
+(val_number) @number
+(val_bool) @bool
+(val_variable) @variable
+(val_nothing) @variable
+(param_type
+  type: (_) @type)
 
 ; Collections
-(record_entry entry_name: (identifier) @property)
+(record_entry key: (identifier) @property)
 
 ; Functions
-(command cmd_name: (identifier) @function)
-(function_definition func_name: (identifier) @function)
-(command arg: (flag_arg) @identifier)
-((block_args block_param: (identifier) @identifier))
+(command head: (cmd_identifier) @function)
+(decl_def (cmd_identifier) @function)
+(command arg_str: (val_string) @identifier)
+((val_closure (parameter_pipes (parameter (identifier) @identifier))))
 
 [
   "and"
@@ -30,10 +32,8 @@
   "if"
   "else"
   "export"
-
-  ; TODO: Once supported by the TS grammar:
-  ; "match"
-  ; "do"
+  "return"
+  "do"
 ] @keyword
 
 [
@@ -60,4 +60,6 @@
 ;       | uniq
 ;       | str join "|"
 ;
-(command cmd_name: (identifier) @function.builtin (#match? @function.builtin "^(alias|all|ansi|any|append|ast|bits|break|bytes|cal|cd|char|clear|collect|columns|commandline|compact|complete|config|const|continue|cp|date|debug|decode|def|default|describe|detect|dfr|do|drop|du|each|echo|encode|enumerate|error|every|exec|exit|explain|explore|export|export-env|extern|fill|filter|find|first|flatten|fmt|for|format|from|generate|get|glob|grid|group|group-by|hash|headers|help|hide|hide-env|histogram|history|http|if|ignore|input|insert|inspect|interleave|into|is-admin|is-empty|is-not-empty|is-terminal|items|join|keybindings|kill|last|lazy|length|let|let-env|lines|load-env|loop|ls|match|math|merge|metadata|mkdir|mktemp|module|move|mut|mv|nu-check|nu-highlight|open|overlay|panic|par-each|parse|path|port|prepend|print|ps|query|random|range|reduce|register|reject|rename|return|reverse|rm|roll|rotate|run-external|save|schema|scope|select|seq|shuffle|skip|sleep|sort|sort-by|source|source-env|split|split-by|start|stor|str|sys|table|take|tee|term|timeit|to|touch|transpose|try|tutor|ulimit|uniq|uniq-by|update|upsert|url|use|values|version|view|watch|where|which|while|whoami|window|with-env|wrap|zip)$"))
+(command head: (cmd_identifier) @function.builtin (#match? @function.builtin "^(alias|all|ansi|any|append|ast|bits|break|bytes|cal|cd|char|clear|collect|columns|commandline|compact|complete|config|const|continue|cp|date|debug|decode|def|default|describe|detect|dfr|do|drop|du|each|echo|encode|enumerate|error|every|exec|exit|explain|explore|export|export-env|extern|fill|filter|find|first|flatten|fmt|for|format|from|generate|get|glob|grid|group|group-by|hash|headers|help|hide|hide-env|histogram|history|http|if|ignore|input|insert|inspect|interleave|into|is-admin|is-empty|is-not-empty|is-terminal|items|join|keybindings|kill|last|lazy|length|let|let-env|lines|load-env|loop|ls|match|math|merge|metadata|mkdir|mktemp|module|move|mut|mv|nu-check|nu-highlight|open|overlay|panic|par-each|parse|path|plugin|port|prepend|print|ps|query|random|range|reduce|register|reject|rename|return|reverse|rm|roll|rotate|run-external|save|schema|scope|select|seq|shuffle|skip|sleep|sort|sort-by|source|source-env|split|split-by|start|stor|str|sys|table|take|tee|term|timeit|to|touch|transpose|try|tutor|ulimit|uname|uniq|uniq-by|update|upsert|url|use|values|version|view|watch|where|which|while|whoami|window|with-env|wrap|zip)$"))
+
+(where_command) @function.builtin
--- config/editor/queries/nu/indents.scm
+++ config/editor/queries/nu/indents.scm
@@ -1,7 +1,8 @@
-(record_or_block) @indent.begin
-(function_definition) @indent.begin
-(if_statement) @indent.begin
-(array) @indent.begin
+(val_record) @indent.begin
+(decl_def (block) @indent.begin)
+(ctrl_if (block) @indent.begin)
+(val_list) @indent.begin
+(parameter_bracks) @indent.begin
 
 [
   "}"
