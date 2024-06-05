95903126b20099e37bcfe125707d652a0f3b59d3
Jesse Gibson <JesseTheGibson@gmail.com>
2024-06-05 10:03:31 -0400

    Implement tab completion
    I pulled this snippet from their wiki with a little modification.

--- config/neovim.lua
+++ config/neovim.lua
@@ -21,6 +21,7 @@ vim.o.autoread = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.mouse = ''
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
 -- Display settings
 vim.o.incsearch = true
@@ -268,12 +269,34 @@ vim.api.nvim_create_autocmd('LspAttach', {
 -- Completion
 local cmp = require('cmp')
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 cmp.setup({
-  mapping = cmp.mapping.preset.insert({}),
+  mapping = cmp.mapping.preset.insert({
+    ['<tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+
+    ['<s-tab>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item()
+      end
+    end, { 'i', 's' }),
+  }),
+
   sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "path" },
-    { name = "buffer" },
+    { name = 'nvim_lsp' },
+    { name = 'path' },
+    { name = 'buffer' },
   }),
 })
 
