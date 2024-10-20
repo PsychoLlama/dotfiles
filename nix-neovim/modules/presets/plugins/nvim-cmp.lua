35a9a28b9f73d3b15a5e5aa02f3f15f0917227ed
Jesse Gibson <JesseTheGibson@gmail.com>
2024-10-20 14:24:55 -0400

    Source completions from all visible buffers
    This has been bugging me for a while. The default behavior is to
    complete from the current buffer. I often have documentation open in
    another window and it's frustrating to copy it manually.

--- nix-neovim/modules/presets/plugins/nvim-cmp.lua
+++ nix-neovim/modules/presets/plugins/nvim-cmp.lua
@@ -1,6 +1,6 @@
 local cmp = require('cmp')
 
-local has_words_before = function()
+local function has_words_before()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
@@ -27,6 +27,16 @@ cmp.setup({
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'path' },
-    { name = 'buffer' },
+    {
+      name = 'buffer',
+      option = {
+        -- Pull completions from all visible buffers.
+        get_bufnrs = function()
+          return vim.iter(vim.api.nvim_tabpage_list_wins(0))
+              :map(vim.api.nvim_win_get_buf)
+              :totable()
+        end,
+      }
+    },
   }),
 })
