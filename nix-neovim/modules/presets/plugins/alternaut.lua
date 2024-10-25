27bb6449a2048431071a4d2585174afbce34b55e
Jesse Gibson <JesseTheGibson@gmail.com>
2024-10-25 00:40:42 -0400

    Remove dead code in alternaut config
    I forgot to remove these duplicated rules after moving them back to Nix.

--- nix-neovim/modules/presets/plugins/alternaut.lua
+++ nix-neovim/modules/presets/plugins/alternaut.lua
@@ -3,27 +3,6 @@ return function(conventions)
     desc = 'Switch between related files',
   })
 
-  local typescript = {
-    directory_naming_conventions = { "__tests__" },
-    file_extensions = { "ts", "tsx", "js", "jsx" },
-    file_naming_conventions = {
-      "{name}.test.{ext}",
-      "{name}.unit.{ext}"
-    }
-  }
-
-  local javascript = {
-    directory_naming_conventions = { "__tests__" },
-    file_extensions = { "js" },
-    file_naming_conventions = { "{name}.test.{ext}" }
-  }
-
-  local vader = {
-    directory_naming_conventions = { "tests" },
-    file_extensions = { "vim", "vader" },
-    file_naming_conventions = { "{name}.{ext}" }
-  }
-
   -- TODO:
   -- 1. Rewrite Alternaut in Lua
   -- 2. Pass a list of rules instead of filetype mappings
