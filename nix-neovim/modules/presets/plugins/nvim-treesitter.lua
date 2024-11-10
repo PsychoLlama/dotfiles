a9e12b17961d50c2ec7b60eba0d476c8b3728b3b
Jesse Gibson <JesseTheGibson@gmail.com>
2024-11-10 14:38:39 -0500

    Enable more treesitter features
    This adds navigation and text objects for functions, methods, and
    classes.

--- nix-neovim/modules/presets/plugins/nvim-treesitter.lua
+++ nix-neovim/modules/presets/plugins/nvim-treesitter.lua
@@ -2,6 +2,27 @@ require('nvim-treesitter.configs').setup({
   highlight = { enable = true },
   indent = { enable = true },
   textobjects = {
+    select = {
+      enable = true,
+      keymaps = {
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true,
+      goto_previous_start = {
+        ['[f'] = '@function.outer',
+        ['[c'] = '@class.outer',
+      },
+      goto_next_start = {
+        [']f'] = '@function.outer',
+        [']c'] = '@class.outer',
+      },
+    },
     swap = {
       enable = true,
       swap_next = {
