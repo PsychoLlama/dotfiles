489da8d4f3ddf9e331919c05f5785630f7490d80
Jesse Gibson <JesseTheGibson@gmail.com>
2024-12-12 21:34:31 -0500

    Disable tmux systemd service
    The equivalent never worked quite right on macOS, which was the whole
    point of it (permissions). Well, that and logs and service monitoring.
    But I discovered that's why `wl-paste` and friends never worked in my
    terminal. Clients inherit the server's environment, and if the systemd
    service starts before the graphical session OR operates in an
    independent environment, it will never receive those environment
    variables.
    
    By removing the tmux server I can finally run those commands again.

--- configs/home-manager/modules/presets/programs/tmux/default.nix
+++ configs/home-manager/modules/presets/programs/tmux/default.nix
@@ -14,73 +14,41 @@ in
 {
   options.presets.programs.tmux.enable = mkEnableOption "Use an opinionated tmux configuration";
 
-  config = mkMerge [
-    (mkIf cfg.enable {
-      home.shellAliases.t = "tmux";
+  config = mkIf cfg.enable {
+    home.shellAliases.t = "tmux";
 
-      programs.tmux = {
-        enable = true;
-        package = pkgs.unstable.tmux;
-        customPaneNavigationAndResize = true;
-        escapeTime = 0;
-        historyLimit = 100000;
-        keyMode = "vi";
-        shell = "${config.programs.nushell.package}/bin/nu";
-        extraConfig = ''
-          ${builtins.readFile ./tmux.conf}
+    programs.tmux = {
+      enable = true;
+      package = pkgs.unstable.tmux;
+      customPaneNavigationAndResize = true;
+      escapeTime = 0;
+      historyLimit = 100000;
+      keyMode = "vi";
+      shell = "${config.programs.nushell.package}/bin/nu";
+      extraConfig = ''
+        ${builtins.readFile ./tmux.conf}
 
-          bind-key C-s display-popup -E ${pkgs.writers.writeBash "tmux-jump" ''
-            sessions="$(${tmux}/bin/tmux list-sessions -F "#{session_name}")"
-            session_name="$(echo -e "$sessions" | ${pkgs.fzf}/bin/fzf)"
+            bind-key C-s display-popup -E ${pkgs.writers.writeBash "tmux-jump" ''
+              sessions="$(${tmux}/bin/tmux list-sessions -F "#{session_name}")"
+              session_name="$(echo -e "$sessions" | ${pkgs.fzf}/bin/fzf)"
 
-            if [[ -n "$session_name" ]]; then
+              if [[ -n "$session_name" ]]; then
               ${tmux}/bin/tmux switch-client -t "$session_name"
-            fi
-          ''}
+              fi
+            ''}
+      '';
+    };
+
+    # The default session variable attempts POSIX interpolation, which
+    # obviously doesn't work in Nushell.
+    programs.nushell.extraEnv =
+      if pkgs.stdenv.isDarwin then
+        ''
+          $env.TMUX_TMPDIR = "/tmp"
+        ''
+      else
+        ''
+          $env.TMUX_TMPDIR = $env.XDG_RUNTIME_DIR
         '';
-      };
-
-      # The default session variable attempts POSIX interpolation, which
-      # obviously doesn't work in Nushell.
-      programs.nushell.extraEnv =
-        if pkgs.stdenv.isDarwin then
-          ''
-            $env.TMUX_TMPDIR = "/tmp"
-          ''
-        else
-          ''
-            $env.TMUX_TMPDIR = $env.XDG_RUNTIME_DIR
-          '';
-    })
-
-    (mkIf (cfg.enable && pkgs.stdenv.isLinux) {
-      systemd.user.services.tmux-server = {
-        Install.WantedBy = [ "default.target" ];
-
-        Unit = {
-          Description = "tmux server";
-          Documentation = "man:tmux(1)";
-        };
-
-        Service = {
-          Type = "oneshot";
-          ExecStart = "${tmux}/bin/tmux start-server";
-          RemainAfterExit = true;
-        };
-      };
-    })
-
-    # Running tmux through launchctl on macOS means I don't need to grant app
-    # permissions to every terminal emulator. Calls route through the owning
-    # process instead.
-    (mkIf (cfg.enable && pkgs.stdenv.isDarwin) {
-      launchd.agents.tmux-server = {
-        enable = true;
-        config = {
-          RunAtLoad = true;
-          ProgramArguments = [ "${tmux}/bin/tmux start-server" ];
-        };
-      };
-    })
-  ];
+  };
 }
