4c581c1bbb626926053761ee0abd722e2edc39cd
Jesse Gibson <JesseTheGibson@gmail.com>
2025-04-17 09:51:46 -0400

    Shift alternaut bindings
    I really like the alt+backslash one. It deserves a more common use.
    Shifted by 1.

--- platforms/editor/modules/psychollama/presets/plugins/alternaut/config.lua
+++ platforms/editor/modules/psychollama/presets/plugins/alternaut/config.lua
@@ -2,19 +2,19 @@ return function(config)
   local alternaut = require('alternaut')
 
   vim.keymap.set('n', '<A-0>', function()
-    alternaut.toggle('test')
-  end)
-
-  vim.keymap.set('n', '<A-9>', function()
     alternaut.toggle('style')
   end)
 
-  vim.keymap.set('n', '<A-8>', function()
+  vim.keymap.set('n', '<A-9>', function()
     alternaut.toggle('template')
   end)
 
-  vim.keymap.set('n', '<A-\\>', function()
+  vim.keymap.set('n', '<A-8>', function()
     alternaut.toggle('header')
+  end)
+
+  vim.keymap.set('n', '<A-\\>', function()
+    alternaut.toggle('test')
   end)
 
   alternaut.setup(config)
