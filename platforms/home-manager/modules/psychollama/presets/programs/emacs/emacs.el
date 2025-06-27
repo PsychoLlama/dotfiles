0cf5490c3b2f32850cd85a5a20b7e8ad6e74d460
Jesse Gibson <JesseTheGibson@gmail.com>
2025-06-27 18:35:04 -0400

    Improve evil mode
    Now I can use evil mode in the minibuffer.

--- platforms/home-manager/modules/psychollama/presets/programs/emacs/emacs.el
+++ platforms/home-manager/modules/psychollama/presets/programs/emacs/emacs.el
@@ -14,17 +14,27 @@
   ; Use a transparent background in terminal mode
   (set-face-background 'default "unspecified-bg"))
 
+; --- ESSENTIAL PLUGINS ---
+
+(projectile-mode 1)
+(counsel-mode 1)
+(company-mode 1)
+
+; --- EVIL MODE ---
+
+(setq evil-want-minibuffer t) ; use vim keybinds in the minibuffer
+(setq evil-want-Y-yank-to-eol t) ; mirror nvim 0.10 `Y` behavior
+
 ; yay, evil!
 (evil-mode 1)
 (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
 (evil-terminal-cursor-changer-activate) ; fix terminal cursor modes
 
+; evil plugins
 (global-evil-surround-mode 1)
 (evil-commentary-mode 1)
 
-(projectile-mode 1)
-(counsel-mode 1)
-(company-mode 1)
+; --- TREESITTER INTEGRATION ---
 
 (global-tree-sitter-mode 1)
 (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
