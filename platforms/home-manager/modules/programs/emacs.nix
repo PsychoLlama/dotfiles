fe61e5b9bf048c150c13e5f54afa076d6cd8957b
Jesse Gibson <JesseTheGibson@gmail.com>
2025-06-28 13:34:13 -0400

    Allow setting individual emacs vars from nix
    In retrospect, this is a better way to configure things. I can tailor
    the data structure for each plugin. Otherwise I'd need to assume deeper
    structure on `df/config` every time I use it.

--- platforms/home-manager/modules/programs/emacs.nix
+++ platforms/home-manager/modules/programs/emacs.nix
@@ -8,27 +8,39 @@
 let
   cfg = config.programs.emacs;
   json = pkgs.formats.json { };
-  settings-payload = json.generate "${cfg.settings-var}.json" cfg.settings;
 in
 
 {
   options.programs.emacs = {
-    settings-var = lib.mkOption {
-      type = lib.types.str;
-      default = "df/config";
-      description = ''
-        The variable name to which the Emacs settings will be bound.
-        This is used to access the settings in Emacs Lisp.
-      '';
-    };
+    variables = lib.mkOption {
+      type = lib.types.attrsOf (
+        lib.types.submodule {
+          options.value = lib.mkOption {
+            type = json.type;
+            default = null;
+            description = "Default value for the variable.";
+          };
 
-    settings = lib.mkOption {
-      type = json.type;
+          options.description = lib.mkOption {
+            type = lib.types.str;
+            default = "Config variable set by Nix.";
+            description = "Doc string for the variable.";
+          };
+        }
+      );
+
       default = { };
       description = ''
-        Settings exposed to the user configuration.
-        Values are bound to the special variable `${cfg.settings-var}`.
+        A set of variables defined in Emacs.
+        All variables belong to the `dotfiles` group.
       '';
+
+      example = {
+        "df/manage-clipboard" = {
+          value = true;
+          description = "Manage the system clipboard.";
+        };
+      };
     };
 
     config-file = lib.mkOption {
@@ -45,10 +57,18 @@ in
     (defgroup dotfiles nil
       "Settings for the dotfiles framework.")
 
-    (defcustom df/config (json-read-file "${settings-payload}")
-      "Config passed by Nix."
-      :type 'sexp
-      :group 'dotfiles)
+    ${lib.pipe cfg.variables [
+      (lib.mapAttrsToList (
+        name: var: ''
+          (defcustom ${name} (json-read-file "${json.generate "${name}.json" var.value}")
+            ${lib.strings.toJSON var.description}
+            :type 'sexp
+            :group 'dotfiles)
+        ''
+      ))
+
+      (lib.concatStringsSep "\n")
+    ]}
 
     ${lib.optionalString (cfg.config-file != null) (builtins.readFile cfg.config-file)}
   '';
