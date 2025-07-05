b36a12b224086e44987b7bac755a56cc1b206616
Jesse Gibson <JesseTheGibson@gmail.com>
2025-07-05 17:53:45 -0400

    Remove emacs launchd service
    After wasting an hour troubleshooting plist files, `log` predicates, and
    "unreadable" file errors, launchd magically started working on its own.
    Some malicious macOS goblin decided my suffering was more entertaining
    when I experienced hope.
    
    Even when the emacs launchd service ran, it couldn't read any
    directories due to macOS disk access restrictions. The only fix would be
    to manually assign the nix hash in a whitelist buried in preferences
    with the side effect of editing an obscure SQLite database. That
    might've been tolerable if Finder was even capable of navigating to that
    file in the nix store.
    
    God, I fucking hate macOS.
    
    I'm just going to run the emacs daemon in a separate tmux session. Then
    I don't have to deal with this bullshit.

--- platforms/home-manager/modules/psychollama/presets/services/emacs.nix
+++ platforms/home-manager/modules/psychollama/presets/services/emacs.nix
@@ -15,24 +15,20 @@ in
     enable = lib.mkEnableOption "Start Emacs as a daemon";
   };
 
-  config = lib.mkIf cfg.enable (
-    lib.mkMerge [
-      {
-        services.emacs = {
-          enable = true;
-          client.enable = lib.mkDefault true;
-          startWithUserSession = lib.mkDefault true;
-          defaultEditor = lib.mkDefault true;
-        };
-      }
+  # Only enabled on Linux. Darwin/launchd are miserable to debug and cause horrible permission issues.
+  # It's infinitely easier to run it in a background tmux session.
+  config = lib.mkIf (cfg.enable && pkgs.stdenv.isLinux) {
+    services.emacs = {
+      enable = true;
+      client.enable = lib.mkDefault true;
+      startWithUserSession = lib.mkDefault true;
+      defaultEditor = lib.mkDefault true;
+    };
 
-      (lib.mkIf pkgs.stdenv.isLinux {
-        systemd.user.services.emacs.Service = {
-          # Override shell to inherit standard nushell environment (i.e. dynamic env vars).
-          ExecStart = lib.mkForce "${nu}/bin/nu --login -c 'emacs --fg-daemon'";
-          Type = lib.mkForce "exec";
-        };
-      })
-    ]
-  );
+    systemd.user.services.emacs.Service = {
+      # Override shell to inherit standard nushell environment (i.e. dynamic env vars).
+      ExecStart = lib.mkForce "${nu}/bin/nu --login -c 'emacs --fg-daemon'";
+      Type = lib.mkForce "exec";
+    };
+  };
 }
