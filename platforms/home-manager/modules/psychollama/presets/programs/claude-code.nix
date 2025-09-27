92636f3a52bd021f92d0a18f165bf65c1911a6c9
Jesse Gibson <JesseTheGibson@gmail.com>
2025-09-27 11:14:54 -0400

    Add reasonable allowlist for Claude

--- platforms/home-manager/modules/psychollama/presets/programs/claude-code.nix
+++ platforms/home-manager/modules/psychollama/presets/programs/claude-code.nix
@@ -19,6 +19,66 @@ in
     package = lib.mkDefault pkgs.unstable.claude-code;
     settings = {
       includeCoAuthoredBy = false;
+      theme = "dark";
+      autoUpdates = false;
+
+      permissions = {
+        allow = [
+          # Mostly read-only, but dangerously allows reading outside the workdir.
+          "Bash(cat:*)"
+          "Bash(date:*)"
+          "Bash(file:*)"
+          "Bash(find:*)"
+          "Bash(grep:*)"
+          "Bash(head:*)"
+          "Bash(ls:*)"
+          "Bash(pwd:*)"
+          "Bash(rg:*)"
+          "Bash(tail:*)"
+          "Bash(wc:*)"
+          "Bash(which:*)"
+
+          "Bash(git add:*)"
+          "Bash(git bisect:*)"
+          "Bash(git blame:*)"
+          "Bash(git diff:*)"
+          "Bash(git fetch:*)"
+          "Bash(git grep:*)"
+          "Bash(git log:*)"
+          "Bash(git ls-remote:*)"
+          "Bash(git remote show:*)"
+          "Bash(git restore:*)"
+          "Bash(git show:*)"
+          "Bash(git status:*)"
+
+          "Bash(gh issue list:*)"
+          "Bash(gh issue view:*)"
+          "Bash(gh pr checks:*)"
+          "Bash(gh pr diff:*)"
+          "Bash(gh pr list:*)"
+          "Bash(gh pr view:*)"
+          "Bash(gh repo list:*)"
+          "Bash(gh repo view:*)"
+          "Bash(gh status:*)"
+
+          # Rust Projects
+          "Bash(cargo build:*)"
+          "Bash(cargo check:*)"
+          "Bash(cargo clippy:*)"
+          "Bash(cargo test:*)" # Potential arbitrary code execution.
+
+          # JavaScript / TypeScript projects
+          "Bash(npm info:*)"
+          "Bash(npm run build:*)"
+          "Bash(npm run fmt:*)"
+          "Bash(npm run lint:*)"
+          "Bash(npm run test:*)" # Potential arbitrary code execution.
+
+          "WebFetch(domain:docs.anthropic.com)"
+          "WebFetch(domain:docs.claude.com)"
+          "WebFetch(domain:platform.openai.com)"
+        ];
+      };
     };
   };
 }
