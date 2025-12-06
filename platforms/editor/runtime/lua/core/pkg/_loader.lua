0030c04c454c5100599db83b1dd1624d648f52e2
Jesse Gibson <JesseTheGibson@gmail.com>
2025-12-06 15:20:44 -0500

    Support function configs in core.pkg
    Allow the plugin config field to be a function in addition to a file
    path, enabling inline configuration without separate files.

--- platforms/editor/runtime/lua/core/pkg/_loader.lua
+++ platforms/editor/runtime/lua/core/pkg/_loader.lua
@@ -4,7 +4,7 @@ local M = {}
 --- @field type 'pack' | 'path' Loading strategy
 --- @field name string Unique plugin name
 --- @field source string Path to the plugin's source code
---- @field config nil | string Optional config file
+--- @field config nil | string | core.pkg.ConfigHook Optional config file or function
 --- @field opts table Given options if the config returns a function
 --- @type core.pkg.Plugin[]
 local plugin_manifest = {}
@@ -28,7 +28,11 @@ function M.eval_configs()
   for _, plugin in ipairs(plugins) do
     if plugin.config then
       --- @type core.pkg.ConfigHook
-      local callback = dofile(plugin.config)
+      local callback = plugin.config
+
+      if type(callback) == 'string' then
+        callback = dofile(callback)
+      end
 
       if type(callback) == 'function' then
         callback(plugin.opts)
