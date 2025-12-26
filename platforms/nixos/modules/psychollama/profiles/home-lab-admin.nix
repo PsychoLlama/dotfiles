8a645993ccd287077449226fbe5363b943e536eb
Jesse Gibson <JesseTheGibson@gmail.com>
2025-12-25 21:32:45 -0500

    Simplify home-lab SSH config to use Tailscale hostnames
    Remove hostname canonicalization and use bare hostnames instead,
    relying on Tailscale DNS for resolution.

--- platforms/nixos/modules/psychollama/profiles/home-lab-admin.nix
+++ platforms/nixos/modules/psychollama/profiles/home-lab-admin.nix
@@ -3,6 +3,14 @@
 let
   inherit (config.psychollama.settings) username;
   cfg = config.psychollama.profiles.home-lab-admin;
+
+  hosts = {
+    rpi4-001 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAyb4vh9xDEEV+30G0UPMTSdtVq3Tyfgl9I9VRwf226v";
+    rpi4-002 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJLMZ6+HaPahE4gGIAWW/uGIl/y40p/rSfIhb5t4G+g9";
+    rpi4-003 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFsNbo3bbm0G11GAbRwnr944AitRyqoQMN4LG7rMsvpK";
+    rpi3-001 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN2VZGgphnMAD5tLG+IHBlBWdlUPNfvYEMDK8OQCrG/A";
+    rpi3-002 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKrGfslz9RlB2EzrTL3SfO/NZB5fPiVXWkK+aQRZrlel";
+  };
 in
 
 {
@@ -29,27 +37,11 @@ in
 
     programs.ssh = {
       extraConfig = ''
-        CanonicalizeHostname yes
-        CanonicalDomains host.nova.selfhosted.city
-        CanonicalizeMaxDots 0
-
-        Host *.host.nova.selfhosted.city
-        User root
+        Host ${lib.concatStringsSep " " (lib.attrNames hosts)}
+          User root
       '';
 
-      knownHosts =
-        lib.mapAttrs'
-          (
-            hostName: publicKey:
-            lib.nameValuePair "${hostName}.host.nova.selfhosted.city" { inherit publicKey; }
-          )
-          {
-            rpi4-001 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAyb4vh9xDEEV+30G0UPMTSdtVq3Tyfgl9I9VRwf226v";
-            rpi4-002 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJLMZ6+HaPahE4gGIAWW/uGIl/y40p/rSfIhb5t4G+g9";
-            rpi4-003 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFsNbo3bbm0G11GAbRwnr944AitRyqoQMN4LG7rMsvpK";
-            rpi3-001 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN2VZGgphnMAD5tLG+IHBlBWdlUPNfvYEMDK8OQCrG/A";
-            rpi3-002 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKrGfslz9RlB2EzrTL3SfO/NZB5fPiVXWkK+aQRZrlel";
-          };
+      knownHosts = lib.mapAttrs (hostName: publicKey: { inherit publicKey; }) hosts;
     };
   };
 }
