0082dc7235c4fea1d8c35e1a7e358cfe36fefc37
Jesse Gibson <JesseTheGibson@gmail.com>
2025-12-26 15:24:27 -0500

    Update home-lab hosts: add nas-001, remove rpi3 devices

--- platforms/nixos/modules/psychollama/profiles/home-lab-admin.nix
+++ platforms/nixos/modules/psychollama/profiles/home-lab-admin.nix
@@ -5,11 +5,10 @@ let
   cfg = config.psychollama.profiles.home-lab-admin;
 
   hosts = {
+    nas-001 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOx6MIH8pVfBi0dckuIgssJO5JzlnEKrJrhNSPs7giTR";
     rpi4-001 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAyb4vh9xDEEV+30G0UPMTSdtVq3Tyfgl9I9VRwf226v";
     rpi4-002 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJLMZ6+HaPahE4gGIAWW/uGIl/y40p/rSfIhb5t4G+g9";
     rpi4-003 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFsNbo3bbm0G11GAbRwnr944AitRyqoQMN4LG7rMsvpK";
-    rpi3-001 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN2VZGgphnMAD5tLG+IHBlBWdlUPNfvYEMDK8OQCrG/A";
-    rpi3-002 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKrGfslz9RlB2EzrTL3SfO/NZB5fPiVXWkK+aQRZrlel";
   };
 in
 
