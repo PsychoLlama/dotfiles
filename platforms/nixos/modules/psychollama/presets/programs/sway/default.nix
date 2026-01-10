c9a4ac367507c7f6643f78ed8a442e469a1c90fd
Jesse Gibson <JesseTheGibson@gmail.com>
2026-01-10 17:57:35 -0500

    Style Sway window title bars with theme colors
    Export theme palette as sway variables and use them to style
    the window decorations visible in tabbed/stacking layouts.

--- platforms/nixos/modules/psychollama/presets/programs/sway/default.nix
+++ platforms/nixos/modules/psychollama/presets/programs/sway/default.nix
@@ -2,6 +2,27 @@
 
 let
   cfg = config.psychollama.presets.programs.sway;
+  theme = config.theme.palette;
+
+  # Export theme colors as sway variables.
+  themeVars = ''
+    set $normal_black ${theme.normal.black}
+    set $normal_red ${theme.normal.red}
+    set $normal_green ${theme.normal.green}
+    set $normal_yellow ${theme.normal.yellow}
+    set $normal_blue ${theme.normal.blue}
+    set $normal_magenta ${theme.normal.magenta}
+    set $normal_cyan ${theme.normal.cyan}
+    set $normal_white ${theme.normal.white}
+    set $bright_black ${theme.bright.black}
+    set $bright_red ${theme.bright.red}
+    set $bright_green ${theme.bright.green}
+    set $bright_yellow ${theme.bright.yellow}
+    set $bright_blue ${theme.bright.blue}
+    set $bright_magenta ${theme.bright.magenta}
+    set $bright_cyan ${theme.bright.cyan}
+    set $bright_white ${theme.bright.white}
+  '';
 in
 
 {
@@ -15,7 +36,13 @@ in
 
     programs.sway = {
       enable = lib.mkDefault true;
-      extraConfig = builtins.readFile ./sway.conf;
+      extraConfig = ''
+        # theme.nix
+        ${themeVars}
+
+        # sway.conf
+        ${builtins.readFile ./sway.conf}
+      '';
 
       input = {
         "type:touchpad" = {
